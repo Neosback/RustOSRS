43b6780e3f16002c003a7895a7b78236fd59466f
Neosback <sythril2010@gmail.com>
2026-09-20 12:59:45 -0500

    Expose static-scene renderer primitives

--- client/rust-renderer/src/lib.rs
+++ client/rust-renderer/src/lib.rs
@@ -9,17 +9,21 @@
 //! into this crate.
 
 pub mod draw;
+pub mod height_map;
 pub mod model_info;
 pub mod packed_vertex;
 pub mod packet;
+pub mod static_scene;
 
 #[cfg(target_arch = "wasm32")]
 mod webgl;
 
 pub use draw::{DrawRange, DrawStats, filter_draw_ranges};
+pub use height_map::HeightMap;
 pub use model_info::{ModelInfo, ModelInfoDrawCommand, create_model_info_texture_data};
 pub use packed_vertex::{PackedVertex, VertexInput};
 pub use packet::{RendererPacketError, validate_draw_ranges, validate_geometry};
+pub use static_scene::{StaticMapState, validate_static_scene_packet};
 
 #[cfg(target_arch = "wasm32")]
 pub use webgl::RustWebGlRenderer;
