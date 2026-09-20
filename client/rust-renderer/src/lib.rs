5c8ab9584041d6f293f5be3aee7e0f4e43b6d6cb
Neosback <sythril2010@gmail.com>
2026-09-20 12:38:24 -0500

    Format Rust renderer exports

--- client/rust-renderer/src/lib.rs
+++ client/rust-renderer/src/lib.rs
@@ -10,16 +10,16 @@
 
 pub mod draw;
 pub mod model_info;
-pub mod packet;
 pub mod packed_vertex;
+pub mod packet;
 
 #[cfg(target_arch = "wasm32")]
 mod webgl;
 
 pub use draw::{DrawRange, DrawStats, filter_draw_ranges};
 pub use model_info::{ModelInfo, ModelInfoDrawCommand, create_model_info_texture_data};
-pub use packet::{RendererPacketError, validate_draw_ranges, validate_geometry};
 pub use packed_vertex::{PackedVertex, VertexInput};
+pub use packet::{RendererPacketError, validate_draw_ranges, validate_geometry};
 
 #[cfg(target_arch = "wasm32")]
 pub use webgl::RustWebGlRenderer;
