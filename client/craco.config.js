b7bc5ec7baaf73ac7ae4e7cb59258842f766c91e
Neosback <sythril2010@gmail.com>
2026-09-21 18:37:48 -0500

    Make CRACO production build PnP-safe

--- client/craco.config.js
+++ client/craco.config.js
@@ -15,11 +15,7 @@ const path = require("path");
 const { execFileSync } = require("child_process");
 
 const JsonMinimizerPlugin = require("json-minimizer-webpack-plugin");
-const evalSourceMapMiddleware = require("react-dev-utils/evalSourceMapMiddleware");
-const noopServiceWorkerMiddleware = require("react-dev-utils/noopServiceWorkerMiddleware");
-const redirectServedPath = require("react-dev-utils/redirectServedPathMiddleware");
 const paths = require("react-scripts/config/paths");
-const express = require("express");
 
 // This package IS the CRA app root (sibling of ../server and ../docs).
 const appRoot = __dirname;
@@ -38,7 +34,7 @@ paths.appBuild = path.resolve(appRoot, "build");
 const interfaceOutput = path.join(paths.appPublic, "browser-host/interfaces");
 fs.mkdirSync(interfaceOutput, { recursive: true });
 const browserHostInterfaces = JSON.parse(
-    execFileSync(process.execPath, [path.join(appRoot, "node_modules/tsx/dist/cli.mjs"), "scripts/browser-host-interface-definitions.ts"], {
+    execFileSync(process.execPath, [require.resolve("tsx/cli"), "scripts/browser-host-interface-definitions.ts"], {
         cwd: path.resolve(appRoot, "../server"),
         encoding: "utf8",
     }),
@@ -152,6 +148,11 @@ module.exports = {
         },
     },
     devServer: (devServerConfig) => {
+        const evalSourceMapMiddleware = require("react-dev-utils/evalSourceMapMiddleware");
+        const noopServiceWorkerMiddleware = require("react-dev-utils/noopServiceWorkerMiddleware");
+        const redirectServedPath = require("react-dev-utils/redirectServedPathMiddleware");
+        const express = require("express");
+
         delete devServerConfig.onBeforeSetupMiddleware;
         delete devServerConfig.onAfterSetupMiddleware;
 
