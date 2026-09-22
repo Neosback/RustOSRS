98e41acd8b6759051072ef0b22bb606858153d7a
Neosback <sythril2010@gmail.com>
2026-09-22 00:04:43 -0500

    Reuse actor data texture storage

--- client/rust-renderer/src/webgl.rs
+++ client/rust-renderer/src/webgl.rs
@@ -535,6 +535,8 @@ pub struct RustWebGlRenderer {
     water_texture_array: WebGlTexture,
     actor_data_texture: WebGlTexture,
     actor_data_actor_capacity: u32,
+    actor_data_texture_width: u32,
+    actor_data_texture_height: u32,
 
     presentation_enabled: bool,
     presentation_framebuffer: WebGlFramebuffer,
@@ -874,6 +876,8 @@ impl RustWebGlRenderer {
             water_texture_array,
             actor_data_texture,
             actor_data_actor_capacity: 0,
+            actor_data_texture_width: 0,
+            actor_data_texture_height: 0,
             presentation_enabled: false,
             presentation_framebuffer,
             presentation_color_texture,
@@ -1326,22 +1330,44 @@ impl RustWebGlRenderer {
         let actor_capacity = u32::try_from(texel_count / 2)
             .map_err(|_| JsValue::from_str("actor-data actor capacity overflow"))?;
 
-        let data = js_sys::Uint16Array::from(values);
+        // WebGL consumes client memory synchronously. Borrow the WASM slice
+        // directly instead of allocating/copying a new JS Uint16Array for every
+        // actor update.
+        let data = unsafe { js_sys::Uint16Array::view(values) };
         self.gl.active_texture(Gl::TEXTURE6);
         self.gl
             .bind_texture(Gl::TEXTURE_2D, Some(&self.actor_data_texture));
-        self.gl
-            .tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_array_buffer_view(
-                Gl::TEXTURE_2D,
-                0,
-                Gl::RGBA16UI as i32,
-                width as i32,
-                height as i32,
-                0,
-                Gl::RGBA_INTEGER,
-                Gl::UNSIGNED_SHORT,
-                Some(data.unchecked_ref()),
-            )?;
+
+        if self.actor_data_texture_width != width || self.actor_data_texture_height != height {
+            self.gl
+                .tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_array_buffer_view(
+                    Gl::TEXTURE_2D,
+                    0,
+                    Gl::RGBA16UI as i32,
+                    width as i32,
+                    height as i32,
+                    0,
+                    Gl::RGBA_INTEGER,
+                    Gl::UNSIGNED_SHORT,
+                    Some(data.unchecked_ref()),
+                )?;
+            self.actor_data_texture_width = width;
+            self.actor_data_texture_height = height;
+        } else {
+            self.gl
+                .tex_sub_image_2d_with_i32_and_i32_and_u32_and_type_and_opt_array_buffer_view(
+                    Gl::TEXTURE_2D,
+                    0,
+                    0,
+                    0,
+                    width as i32,
+                    height as i32,
+                    Gl::RGBA_INTEGER,
+                    Gl::UNSIGNED_SHORT,
+                    Some(data.unchecked_ref()),
+                )?;
+        }
+
         self.actor_data_actor_capacity = actor_capacity;
         Ok(())
     }
